3149b4bc6420843bbc7486ef68a3ec76d17a0a38
Isi-Te <isabell.teuber@gmail.com>
2023-04-01 16:22:04 -0400

    Update styling.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,14 +3,14 @@ import './Navigation.scss';
 import compassIcon from '../../assets/icons/compass-white-icon.svg';
 import plusIcon from '../../assets/icons/add-white-icon.svg';
 import profileIcon from '../../assets/icons/profile-white-icon.svg';
-
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
         <section className='nav'>
-            <img className='nav__icon-compass' src={compassIcon} alt='Compass icon'></img>
-            <img className='nav__icon-track' src={plusIcon} alt='Plus Icon'></img>
-            <img className='nav__icon-profile' src={profileIcon} alt='Profile Icon'></img>
+            <Link to='/'><img className='nav__icon-compass' src={compassIcon} alt='Compass icon'></img></Link>
+            <Link to='/journal'><img className='nav__icon-track' src={plusIcon} alt='Plus Icon'></img></Link>
+            <Link to='/profile'><img className='nav__icon-profile' src={profileIcon} alt='Profile Icon'></img></Link>
         </section>
     );
 };
