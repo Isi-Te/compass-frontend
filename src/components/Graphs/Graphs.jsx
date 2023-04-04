1ecbeb23ee0a8797f1436abf15b256b03b906825
Isi-Te <isabell.teuber@gmail.com>
2023-04-04 12:02:03 -0400

    Set-up graphs and get data from db.

--- src/components/Graphs/Graphs.jsx
+++ src/components/Graphs/Graphs.jsx
@@ -17,11 +17,73 @@ const Graphs = () => {
             .catch(error => console.error(error));
     }, []);
 
+    if (!graphData) {
+        return <div>Loading....</div>
+    }
+
+    // const [newGraphData] = graphData;
+    // const objectArray = Object.entries(newGraphData);
+    // console.log(objectArray);
+    // objectArray.forEach(([key, value]) => {
+    //     console.log(key);
+    //     console.log(value);
+    // })
+
+    console.log(graphData);
+
+    let happy = [];
+    let sad = [];
+    let laughs = [];
+    let frustrated = [];
+    let stress = [];
+    let sleep = [];
+    let nutrition = [];
+    let energy = [];
+    let outdoors = [];
+    let fitness = [];
+    let social = [];
+    let selfCare = [];
+    let timestamp = [];
+
+    graphData.map((category) => {
+        happy.push(category.happy);
+        sad.push(category.sad);
+        laughs.push(category.laughs);
+        frustrated.push(category.frustrated);
+        stress.push(category.stress);
+        sleep.push(category.sleep);
+        nutrition.push(category.nutrition);
+        energy.push(category.energy);
+        outdoors.push(category.outdoors);
+        fitness.push(category.fitness);
+        social.push(category.social);
+        selfCare.push(category.selfCare);
+        timestamp.push(category.created_at);
+    })
+
+    let dates = []
+    timestamp.map((date) => {
+        dates.push(date.slice(5, 10))
+    })
+    console.log(dates)
+
     return (
         <>
-            <LineChart graphData={graphData} />
+            <LineChart className="graph" graphData={graphData} type={happy} name='Happy' dates={dates} />
+            <LineChart graphData={graphData} type={sad} name='Sad' />
+            <LineChart graphData={graphData} type={laughs} name='Laughs' />
+            <LineChart graphData={graphData} type={frustrated} name='Frustrated' />
+            <LineChart graphData={graphData} type={stress} name='Stress level' />
+            <LineChart graphData={graphData} type={sleep} name='Sleep' />
+            <LineChart graphData={graphData} type={nutrition} name='Nutrition' />
+            <LineChart graphData={graphData} type={energy} name='Energy Levels' />
+            <LineChart graphData={graphData} type={outdoors} name='Time outdoors' />
+            <LineChart graphData={graphData} type={fitness} name='Fitness' />
+            <LineChart graphData={graphData} type={social} name='Social' />
+            <LineChart graphData={graphData} type={selfCare} name='Self-care' />
         </>
     );
+
 };
 
 export default Graphs;
--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { Chart } from 'chart.js/auto';
 import { Line } from "react-chartjs-2";
 
-const LineChart = ({ graphData }) => {
+const LineChart = ({ type, name, dates }) => {
 
-    console.log(graphData);
+    console.log({ type });
+    console.log({ name });
+    console.log({ dates });
 
     const data = {
-        labels: ["January", "February", "March", "April", "May", "June"],
+        labels: ['03-30', '03-31', '04-01', '04-02', '04-03', '04-04'],
         datasets: [
             {
-                label: "Stress",
+                label: `${name}`,
                 backgroundColor: "#6FBBB1",
                 borderColor: "#6FBBB1",
-                data: [1, 2, 6, 4, 5, 8],
+                data: `${type}`,
             },
         ],
     };
